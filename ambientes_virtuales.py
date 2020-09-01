f02dcfdc6132391de23e72b0be8577f916cf0f0d
Juan Aguilar <jfar1988@misena.edu.co>
2020-09-01 16:58:48 -0500

    ambientes virtuales

---
+++ ambientes_virtuales.py
@@ -0,0 +1,7 @@
+#Permiten aislar el ambiente para poder instalar diversas versiones de paquetes
+#a partir de python 3 se incluye en la libreria estandar en el modulo venv
+#ningun ingeniero preofesional de python trabaja sin ellos
+# PIP 
+# permite descargar paquetes de terceros para utilizar en nuestro programa
+#permite compartir nuestros paquetes con terceros.
+#permite especificar la version del paquete que necesitamos
